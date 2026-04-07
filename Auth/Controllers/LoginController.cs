9ce7982c5fae13c490299ce0d5a0d7b4c98d03fa
Sayan0361 <sayansen0361@gmail.com>
2026-04-07 17:20:45 +0530

    Auth Service and MainApp connected

---
+++ Auth/Controllers/LoginController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+
+namespace Auth.Controllers
+{
+    public class LoginController : Controller
+    {
+        [HttpPost]
+        public ActionResult Verify(string orgName, string userEmail)
+        {
+            return Content($"Auth Project Received! <br/> " +
+                           $"Org: {orgName} <br/> " +
+                           $"User: {userEmail}");
+        }
+    }
+}
