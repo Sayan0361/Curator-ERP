b260e84d20a30dc7ac761133fa0e112e90a06d08
Subhradeep <subhradeepbasu2002@gmail.com>
2026-04-08 14:39:52 +0530

    WIP: Email Service

---
+++ EmailService/Controllers/EmailController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using EmailService.Models;
+using System.Web.Services.Description;
+
+namespace EmailService.Controllers
+{
+    public class EmailController : Controller
+    {
+        // GET: Email
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<object> SendEmail(EmailModel emailModel)
+        {
+            try
+            {
+                var message = new MailMessage();
+
+                message.To.Add(new MailAddress(
+                    emailModel.NameTo + " <" + emailModel.EmailTo + " >"));
+                message.From = new MailAddress(
+                    emailModel.NameFrom + " <" + emailModel.EmailFrom + " >");
+
+                message.Subject = emailModel.EmailSubject;
+
+                message.Body = emailModel.EmailBody;
+                message.IsBodyHtml = false;
+
+                using (var smtp = new SmtpClient())
+                {
+                    await smtp.SendMailAsync(message);
+                }
+
+                return new
+                {
+                    success = true,
+                    message = "Mail sent successfully"
+                };
+            }
+            catch(Exception ex)
+            {
+                return new
+                {
+                    success = false,
+                    message = "Mail sent failed",
+                    error = ex.ToString()
+                };
+            }
+        }
+    }
+}
---
+++ EmailService/Models/EmailModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EmailService.Models
+{
+    public class EmailModel
+    {
+        [DisplayName("To")]
+        public string NameTo { get; set; }
+
+        [Required]
+        [DisplayName("To Email")]
+        [EmailAddress]
+        public string EmailTo { get; set; }
+        [Required]
+        [DisplayName("Subject")]
+        public string EmailSubject { get; set; }
+        [Required]
+        [DisplayName("Body")]
+        public string EmailBody { get; set; }
+
+        public long OTP {  get; set; }
+        [EmailAddress]
+        [DisplayName("Your Email")]
+        public string EmailFrom { get; set; }
+        public string NameFrom { get; set; }
+    }
+}
