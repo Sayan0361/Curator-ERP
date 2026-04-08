8cb7f074020f2fc3f00e192e8a4fae06b9afc671
Subhradeep <subhradeepbasu2002@gmail.com>
2026-04-08 17:13:00 +0530

    FEATURE: Email Service Done

--- EmailService/Controllers/EmailController.cs
+++ EmailService/Controllers/EmailController.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using EmailService.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+using System.Net;
 using System.Net.Mail;
 using System.Threading.Tasks;
-using EmailService.Models;
+using System.Web;
+using System.Web.Mvc;
 using System.Web.Services.Description;
 
 namespace EmailService.Controllers
@@ -18,6 +19,7 @@ namespace EmailService.Controllers
             return View();
         }
 
+        [HttpPost]
         public async Task<object> SendEmail(EmailModel emailModel)
         {
             try
@@ -25,35 +27,56 @@ namespace EmailService.Controllers
                 var message = new MailMessage();
 
                 message.To.Add(new MailAddress(
-                    emailModel.NameTo + " <" + emailModel.EmailTo + " >"));
+                    emailModel.NameTo + " <" + emailModel.EmailTo + ">"));
+
                 message.From = new MailAddress(
-                    emailModel.NameFrom + " <" + emailModel.EmailFrom + " >");
+                    emailModel.NameFrom + " <" + emailModel.EmailFrom + ">");
 
                 message.Subject = emailModel.EmailSubject;
-
                 message.Body = emailModel.EmailBody;
                 message.IsBodyHtml = false;
 
                 using (var smtp = new SmtpClient())
                 {
-                    await smtp.SendMailAsync(message);
+                    smtp.Send(message);
                 }
 
                 return new
                 {
                     success = true,
-                    message = "Mail sent successfully"
+                    message = "Mail sent successfully",
+                    error = ""
                 };
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 return new
                 {
                     success = false,
                     message = "Mail sent failed",
-                    error = ex.ToString()
+                    error = ex.Message
                 };
             }
+        }
+
+        public async Task<ActionResult> Test()
+        {
+            EmailModel model = new EmailModel
+            {
+                EmailFrom = "subhradeepbasu2305@gmail.com",
+                EmailTo = "subhradeepbasu2002@gmail.com",
+                OTP = 123456,
+                EmailBody = "Here is your OTP: 123456",
+                EmailSubject = "Don't Reply. Just a Test mail.",
+                NameFrom = "Curio",
+                NameTo = "Client"
+            };
+
+            dynamic response = await SendEmail(model);
+
+            return Content($"Success: {response.success} <br/> " +
+                           $"Message: {response.message} <br/> " +
+                           $"Error: {response.error}");
         }
     }
 }
